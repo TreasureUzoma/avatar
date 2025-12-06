d2baa840e1181dc3d77ef139d64edd0cbfba061e
Treasure Uzoma <treasureuzoma650@gmail.com>
2025-12-06 14:58:55 +0100

    fix(image-preview): remove default avatar fallback logic

--- src/lib/components/image-preview.svelte
+++ src/lib/components/image-preview.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import * as Avatar from '$lib/components/ui/avatar';
-	import { meta } from '$lib/meta';
 	import { generateDeterministicNumber } from '$lib/deterministic-hash';
 
 	interface Props {
@@ -13,7 +12,7 @@
 <Avatar.Root class="flex-shrink-0 rounded-full w-[11rem] md:w-[14rem] h-[11rem] md:h-[14rem]">
 	<Avatar.Image
 		alt="Gradient image {text}"
-		src="/avatars/{generateDeterministicNumber(text ?? meta.developer.nickname)}.png"
+		src="/avatars/{generateDeterministicNumber(text)}.png"
 	/>
-	<Avatar.Fallback>i</Avatar.Fallback>
+	<Avatar.Fallback></Avatar.Fallback>
 </Avatar.Root>
